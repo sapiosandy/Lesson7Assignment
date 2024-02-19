cfbf9579e0efab1c494600109ffb0a63fe990611
Sandy Gomez <sgomez1029@gmail.com>
2024-02-19 14:37:49 -0800

    Initial Commit

---
+++ Lesson7Assignment/Lesson7AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson7AssignmentApp.swift
+//  Lesson7Assignment
+//
+//  Created by Sandra Gomez on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Lesson7AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
